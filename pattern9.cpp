bb06dd346925f027f03c1d58a9fa0dbd19c74e4c
ujjwal765 <70845627+ujjwal765@users.noreply.github.com>
2022-01-13 12:49:59 +0530

    Create pattern9.cpp

---
+++ pattern9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int even(int n,int n1)
+{
+    if(n>n1)
+    return 0;
+    cout<<n<<" ";
+    return even(n+2,n1);
+
+}
+int main()
+{
+    int n=1,n1;
+    cin>>n1;
+    even(n,n1);
+}
+
+
